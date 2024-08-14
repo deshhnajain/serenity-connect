d3dfbe99bd71ff647b2aa57258e714f1ffda5e0b
SeaSeekers <seaseekerssih@gmail.com>
2024-08-14 21:12:59 +0530

    Db switched to mongodb atlas

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+dotenv.config(); // Load environment variables from .env file
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // Use the environment variable for the connection string
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
