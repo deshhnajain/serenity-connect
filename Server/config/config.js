c7ac288d3e6554d655031bb7469537a1e268a648
priyam nagar <117069343+priyamnagar266@users.noreply.github.com>
2024-08-14 18:55:01 +0530

    Update config.js

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,11 +1,25 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/test', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log('MongoDB connected...');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/test', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); 
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
