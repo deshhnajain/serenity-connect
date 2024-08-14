2e17fd75f7e7841186a3eba92984377b8500dcb2
Priyam Nagar <lovenagar266@gmail.com>
2024-08-14 19:52:14 +0530

    db

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,25 +1,9 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect('mongodb://localhost:27017/test', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     });
-//     console.log('MongoDB connected...');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); 
+    await mongoose.connect(process.env.MONGO_URI); // Use the environment variable for the connection string
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
