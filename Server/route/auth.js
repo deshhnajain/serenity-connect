04925b2fcce4047925d02d16ea4d5ffd8833290e
Priyam Nagar <lovenagar266@gmail.com>
2024-08-15 13:45:59 +0530

    deployment

--- Server/route/auth.js
+++ Server/route/auth.js
@@ -10,7 +10,7 @@ router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/user-login' }),
   (req, res) => {
     const { jwtToken } = req.user;
-    res.redirect(`http://localhost:3000/home?token=${jwtToken}`);
+    res.redirect(`https://serenity-connect.netlify.app/home?token=${jwtToken}`);
   }
 );
 
